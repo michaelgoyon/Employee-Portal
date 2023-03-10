7b87fa03f1c472cab6395f924bbf0d0238cac209
mjgoyon <michaelgoyon@gmail.com>
2023-03-10 10:40:00 +0800

    Update Employee-Portal-DB.sql

--- Employee-Portal-DB.sql
+++ Employee-Portal-DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 24, 2023 at 03:58 AM
+-- Generation Time: Jan 26, 2023 at 02:00 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.4
 
